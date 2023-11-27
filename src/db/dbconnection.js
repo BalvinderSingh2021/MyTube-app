a1e9cc8cd241e2ab2a48c4b75e58feff64bd0ceb
Er Balvinder Singh <77532854+BalvinderSingh2021@users.noreply.github.com>
2023-11-27 12:26:51 +0530

    ramove console log

--- src/db/dbconnection.js
+++ src/db/dbconnection.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log("connectionInstance", connectionInstance);
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
